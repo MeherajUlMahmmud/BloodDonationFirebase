d66f24f486bf16621fb39f5053d06d311e86d84a
Meheraj <meherajmahmmd@gmail.com>
2021-09-06 16:17:05 +0600

    Update RequestDetailsActivity.java

--- app/src/main/java/com/example/blooddonationfirebase/RequestDetailsActivity.java
+++ app/src/main/java/com/example/blooddonationfirebase/RequestDetailsActivity.java
@@ -3,7 +3,9 @@ package com.example.blooddonationfirebase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -31,12 +33,10 @@ import java.util.List;
 public class RequestDetailsActivity extends AppCompatActivity {
 
     private String id;
-    private TextView patientName_tv, postedOn_tv, phone_tv, location_tv, bloodGroup_tv, neededWithin_tv, note_tv, user_tv;
-    private Button showOnMap_btn, contact_btn;
+    private TextView patientName_tv, postedOn_tv, phone_tv, location_tv, bloodGroup_tv, neededWithin_tv, note_tv, unit_tv, user_tv;
+    private Button showOnMap_btn, contact_btn, update_btn, delete_btn;
     private Request req;
 
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private ProgressDialog pd;
 
     @Override
@@ -71,13 +71,47 @@ public class RequestDetailsActivity extends AppCompatActivity {
             Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
             startActivity(intent1);
         });
+
+        update_btn.setOnClickListener(v -> {
+            Intent i = new Intent(RequestDetailsActivity.this, ModifyRequestActivity.class);
+            i.putExtra("reqId", id);
+            startActivity(i);
+        });
+
+        delete_btn.setOnClickListener(v -> showDeleteDialog());
+    }
+
+    private void showDeleteDialog() {
+        String message = "Are you sure about deleting this request?";
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message).setTitle("Deleting Blood Request");
+
+        //Setting message manually and performing action on button click
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        deleteRequest();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private void fetchData(String id) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
         db.collection("requests").document(id).get()
                 .addOnCompleteListener(task -> {
                     req = new Request();
 
+                    System.out.println("Here");
                     req.setId(task.getResult().getString("id"));
                     req.setPatientName(task.getResult().getString("patientName"));
                     req.setGender(task.getResult().getString("gender"));
@@ -91,7 +125,6 @@ public class RequestDetailsActivity extends AppCompatActivity {
                     req.setUserId(task.getResult().getString("userId"));
 
                     showData(req);
-
                 })
                 .addOnFailureListener(e -> {
                     Toast.makeText(RequestDetailsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -101,16 +134,18 @@ public class RequestDetailsActivity extends AppCompatActivity {
     private void showData(Request req) {
         patientName_tv.setText(req.getPatientName());
         postedOn_tv.setText(req.getPostedOn());
-        phone_tv.setText(req.getPhone());
         location_tv.setText(req.getLocation());
         bloodGroup_tv.setText(req.getBloodGroup());
+        unit_tv.setText("(" + req.getUnit() + ") Unit(s)");
         neededWithin_tv.setText(req.getNeededWithin());
         note_tv.setText(req.getNote());
         phone_tv.setText(req.getPhone());
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("profiles").document(req.getUserId()).get()
                 .addOnCompleteListener(task -> {
                     pd.dismiss();
+
                     user_tv.setText(task.getResult().getString("name"));
                 })
                 .addOnFailureListener(e -> {
@@ -118,16 +153,49 @@ public class RequestDetailsActivity extends AppCompatActivity {
                     Toast.makeText(RequestDetailsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        System.out.println("User Id " + user.getUid());
+        System.out.println(user.getUid().length());
+        System.out.println("Req User Id " + req.getUserId());
+        System.out.println(req.getUserId().length());
+
+        if (req.getUserId().equals(user.getUid())) {
+            update_btn.setVisibility(View.VISIBLE);
+            delete_btn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void deleteRequest() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("requests").document(id).delete()
+                .addOnCompleteListener(task -> {
+                    Toast.makeText(RequestDetailsActivity.this, "Request Deleted", Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(RequestDetailsActivity.this, MainActivity.class);
+                    startActivity(i);
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(RequestDetailsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                this.finish();
+                Intent i = new Intent(RequestDetailsActivity.this, MainActivity.class);
+                startActivity(i);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent i = new Intent(RequestDetailsActivity.this, MainActivity.class);
+        startActivity(i);
     }
 
     private void initializeViews() {
@@ -136,11 +204,14 @@ public class RequestDetailsActivity extends AppCompatActivity {
         phone_tv = findViewById(R.id.phone_tv);
         location_tv = findViewById(R.id.location_tv);
         bloodGroup_tv = findViewById(R.id.bloodGroup_tv);
+        unit_tv = findViewById(R.id.unit_tv);
         neededWithin_tv = findViewById(R.id.neededWithin_tv);
         note_tv = findViewById(R.id.note_tv);
         user_tv = findViewById(R.id.user_tv);
 
         contact_btn = findViewById(R.id.contact_btn);
         showOnMap_btn = findViewById(R.id.showOnMap_btn);
+        update_btn = findViewById(R.id.update_btn);
+        delete_btn = findViewById(R.id.delete_btn);
     }
 }
