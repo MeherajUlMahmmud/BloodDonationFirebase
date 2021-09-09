04fbf8c970fdd872c7ae0e9ef4c202cbc97ecb79
Meheraj <meherajmahmmd@gmail.com>
2021-09-09 22:20:10 +0600

    Update DonorsFragment.java

--- app/src/main/java/com/example/blooddonationfirebase/main_fragments/DonorsFragment.java
+++ app/src/main/java/com/example/blooddonationfirebase/main_fragments/DonorsFragment.java
@@ -1,44 +1,49 @@
 package com.example.blooddonationfirebase.main_fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.blooddonationfirebase.R;
+import com.example.blooddonationfirebase.models.Donor;
+import com.example.blooddonationfirebase.models.Request;
+import com.example.blooddonationfirebase.utils.DonorAdapter;
+import com.example.blooddonationfirebase.utils.RequestAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DonorsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class DonorsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView donor_recycler;
+
+    private ProgressDialog pd;
 
     public DonorsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DonorsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DonorsFragment newInstance(String param1, String param2) {
         DonorsFragment fragment = new DonorsFragment();
         Bundle args = new Bundle();
@@ -60,7 +65,57 @@ public class DonorsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donors, container, false);
+        View view = inflater.inflate(R.layout.fragment_donors, container, false);
+        donor_recycler = view.findViewById(R.id.donor_recycler);
+        donor_recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        getAllDonors(view);
+
+        return view;
+    }
+
+    public void getAllDonors(View view) {
+        pd = new ProgressDialog(getActivity());
+        pd.setTitle("Loading...");
+        pd.setCancelable(false);
+        pd.setCanceledOnTouchOutside(false);
+        pd.getWindow().setGravity(Gravity.CENTER);
+        pd.show();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference requestsCollectionRef = db
+                .collection("profiles");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Query donorQuery = requestsCollectionRef
+                .whereNotEqualTo("id", user.getUid());
+
+        donorQuery.get()
+                .addOnCompleteListener(task -> {
+                    pd.dismiss();
+
+//                    System.out.println("Fetched");
+                    List<Donor> donorList = new ArrayList<>();
+                    for (DocumentSnapshot doc : task.getResult()) {
+                        Donor donor = new Donor();
+                        donor.setId(doc.getString("id"));
+                        donor.setName(doc.getString("name"));
+                        donor.setGender(doc.getString("gender"));
+                        donor.setBloodGroup(doc.getString("bloodGroup"));
+                        donor.setPhone(doc.getString("phone"));
+                        donor.setLocation(doc.getString("location"));
+                        donor.setLastDonation(doc.getString("lastDonation"));
+                        donor.setAvailable(doc.getString("available"));
+
+                        donorList.add(donor);
+//                        System.out.println("added");
+                    }
+                    donor_recycler.setAdapter(new DonorAdapter(getContext(), donorList));
+                })
+                .addOnFailureListener(e -> {
+                    pd.dismiss();
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
     }
 }
